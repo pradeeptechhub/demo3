d20ff1ecb508c289e2c35b945098b0ad471c80e2
pradeep <pradeep.techhub@gmail.com>
2016-04-23 17:32:10 +0530

    ------ Test Concludes ------

--- src/main/webapp/resources/js/resource.js
+++ src/main/webapp/resources/js/resource.js
@@ -1,11 +1,13 @@
-$(document).ready(function(){
 
-	$("#request-link").click(function(e){
-		e.preventDefault();
-		$.post(ctx + "/resource/request", $("form").serialize(), function(data){
-			alert(data);
+$(document).ready(
+	function() {
+		$("#request-link").click(
+			function(e) {
+				e.preventDefault();
+				$.post(ctx + "/resource/request",
+				$("form").serialize(), function(data) {
+						alert(data);
+				});
+				return false;
 		});
-		return false;
-	});
-
 });
